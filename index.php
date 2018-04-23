ce43a7f2460a618b67d143120f22abc35878a047
smboy86 <smboy86@naver.com>
2018-04-23 16:45:03 +0900

    naver kin 180423

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php 
-    echo "<script>location.href='/naver/180417_php/indexQ2.php';</script>";
+    // 현재 보려는 php 주소를 입력한다.
+    $php_url = "/naver/180423_php/index.php";
+    echo "<script>location.href='$php_url';</script>";
 ?>
